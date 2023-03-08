fc6313dc482b97c4d09e66fd1c2b362e95f6d0f0
Nada Hesham <88733032+46837@users.noreply.github.com>
2023-03-08 19:32:14 +0200

    Add files via upload

---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,495 @@
+// all images imported from images directory
+import product_01_image_01 from "../15-300x300.jpg";
+import product_01_image_02 from "../16.jpg";
+import product_01_image_03 from "../14.jpg";
+
+import product_02_image_01 from "../8-300x300.jpg";
+import product_02_image_02 from "../FRY-SHRIMPS-.jpg";
+import product_02_image_03 from "../FRY-CALAMARI-.jpg";
+
+import product_03_image_01 from "../38-1536x1536.jpg";
+
+
+import product_04_image_01 from "../5.jpg";
+
+
+import product_05_image_01 from "../1.jpg";
+import product_05_image_02 from "../Grilled-shrimp1.jpg";
+import product_05_image_03 from "../12.jpg";
+
+import product_06_image_01 from "../Tom-yum-soup-special.jpg";
+import product_06_image_02 from "../s3-300x300.jpg";
+import product_06_image_03 from "../CRAB-SOUP-.jpg";
+
+import im from "../FRY-JUNIOR-BOX-.jpg";
+import img from "../Grilled-Junior-Box.jpg";
+
+import one from "../LOBSTER-DYNAMITE-.jpg";
+
+
+
+
+
+
+const products = [
+  {
+    id: "01",
+    title: "Greek Salad",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: " Salads",
+
+    des:"no description"
+  },
+  {
+    id: "03",
+    title: "GRILLED KING FISH",
+    price: 110.0,
+    image01: product_03_image_01,
+    
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "04",
+    title: "CRAB GRILLED",
+    price: 110.0,
+    image01: product_04_image_01,
+   
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "05",
+    title: "GRILLED SHRIMPS",
+    price: 240.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "GRILLED",
+
+    des:"no description"
+  },
+  {
+    id: "06",
+    title: "CRAB SOUP",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "SOUP",
+
+    des:"no description"
+  },
+
+  {
+    id: "07",
+    title: "FRIED BOX",
+    price: 170.0,
+    image01: im,
+    image02: img,
+    
+    category: " JUNIOR BOX",
+
+    des:"no description"
+  },
+
+
+  {
+    id: "10",
+    title: "Classic Hamburger",
+    price: 24.0,
+    image01: product_05_image_02,
+    image02: product_05_image_01,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    des:"no description"
+  },
+
+  {
+    id: "11",
+    title: "Crunchy Bread ",
+    price: 35.0,
+    image01: product_06_image_01,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+
+  {
+    id: "12",
+    title: "Delicious Bread ",
+    price: 35.0,
+    image01: product_06_image_02,
+    image02: product_06_image_01,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+  {
+    id: "01",
+    title: "Greek Salad",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: " Salads",
+
+    des:"no description"
+  },
+
+  {
+    id: "02",
+    title: "FRIED SHERRY FISH",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "FRIED",
+
+    des:"no description"
+  },
+
+  {
+    id: "03",
+    title: "GRILLED KING FISH",
+    price: 110.0,
+    image01: product_03_image_01,
+    
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "04",
+    title: "CRAB GRILLED",
+    price: 110.0,
+    image01: product_04_image_01,
+   
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "05",
+    title: "GRILLED SHRIMPS",
+    price: 240.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "GRILLED",
+
+    des:"no description"
+  },
+  {
+    id: "06",
+    title: "CRAB SOUP",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "SOUP",
+
+    des:"no description"
+  },
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 350.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+  {
+    id: "01",
+    title: "Greek Salad",
+    price: 204.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: " Salads",
+
+    des:"no description"
+  },
+
+  {
+    id: "02",
+    title: "FRIED SHERRY FISH",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "FRIED",
+
+    des:"no description"
+  },
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+  {
+    id: "01",
+    title: "Greek Salad",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: " Salads",
+
+    des:"no description"
+  },
+
+  {
+    id: "02",
+    title: "FRIED SHERRY FISH",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "FRIED",
+
+    des:"no description"
+  },
+  {
+    id: "03",
+    title: "GRILLED KING FISH",
+    price: 110.0,
+    image01: product_03_image_01,
+    
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "04",
+    title: "CRAB GRILLED",
+    price: 110.0,
+    image01: product_04_image_01,
+   
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "05",
+    title: "GRILLED SHRIMPS",
+    price: 240.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "GRILLED",
+
+    des:"no description"
+  },
+  {
+    id: "06",
+    title: "CRAB SOUP",
+    price: 240.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "SOUP",
+
+    des:"no description"
+  },
+
+  {
+    id: "07",
+    title: "FRIED BOX",
+    price: 170.0,
+    image01: im,
+    image02: img,
+    
+    category: " JUNIOR BOX",
+
+    des:"no description"
+  },
+
+
+  {
+    id: "10",
+    title: "Classic Hamburger",
+    price: 24.0,
+    image01: product_05_image_02,
+    image02: product_05_image_01,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    des:"no description"
+  },
+
+  {
+    id: "11",
+    title: "Crunchy Bread ",
+    price: 35.0,
+    image01: product_06_image_01,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+
+  {
+    id: "12",
+    title: "Delicious Bread ",
+    price: 35.0,
+    image01: product_06_image_02,
+    image02: product_06_image_01,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+  {
+    id: "01",
+    title: "Greek Salad",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: " Salads",
+
+    des:"no description"
+  },
+
+  {
+    id: "02",
+    title: "FRIED SHERRY FISH",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "FRIED",
+
+    des:"no description"
+  },
+
+  {
+    id: "03",
+    title: "GRILLED KING FISH",
+    price: 110.0,
+    image01: product_03_image_01,
+    
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "04",
+    title: "CRAB GRILLED",
+    price: 110.0,
+    image01: product_04_image_01,
+   
+    category: "GRILLED",
+
+    des:"no description"
+  },
+
+  {
+    id: "05",
+    title: "GRILLED SHRIMPS",
+    price: 240.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "GRILLED",
+
+    des:"no description"
+  },
+  {
+    id: "06",
+    title: "CRAB SOUP",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "SOUP",
+
+    des:"no description"
+  },
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    des:"no description"
+  },
+  {
+    id: "01",
+    title: "Greek Salad",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: " Salads",
+    des:"no description"
+
+  },
+
+  {
+    id: "02",
+    title: "FRIED SHERRY FISH",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "FRIED",
+des:"no description"
+  },
+];
+
+export default products;
